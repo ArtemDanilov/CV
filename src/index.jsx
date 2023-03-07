ca7e1cdc122cc71154a65654619e2eeec19aca62
ArtemD <artemdanilow95@gmail.com>
2023-03-07 12:30:04 +0100

    Install tailwindcss

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from './pages/Home';
+import './scss/tailwind.scss'
 
 const router = createBrowserRouter([
   {
