2ef491045f6aef92eb95259a2b4d8a2848e07166
ArtemD <artemdanilow95@gmail.com>
2023-03-10 21:15:55 +0100

    Add title decor for MainContainer

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Container = ({ children, title }) => {
-    const before = '//';
-
     return (
         <div className="mt-1 space-y-4">
-            <h2 className="text-3xl font-bold font-mono text-zinc-700 mb-8">
-                { before } { title }
+            <h2 className="
+                flex items-center text-3xl font-bold font-mono text-zinc-700 mb-8
+                after:content-[''] after:block after:w-full after:mx-4 after:h-0.5 after:bg-zinc-700"
+            >
+                { title }
             </h2>
 
             { children }
