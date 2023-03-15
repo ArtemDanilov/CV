67e244108ff249257d9e906839a518dd7b861a1d
ArtemD <artemdanilow95@gmail.com>
2023-03-15 19:01:58 +0100

    Add About me section and change some data

---
+++ src/pages/sections/Main/About/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Container from '../../../../components/Container';
+
+const About = (props) => {
+    const data = props.content.about;
+
+    return (
+        <section className="experience w-full p-4 lg:pl-8">
+            <Container type="advanced" title={ data.title } key={ data.id }>
+                <p>{ data.text }</p>
+            </Container>
+        </section>
+    )
+}
+
+export default About;
--- src/pages/sections/Main/index.jsx
+++ src/pages/sections/Main/index.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 
 import Experience from './Experience/index';
 import Education from './Education/index';
+import About from './About/index';
 
 const Main = (props) => {
     const data = props.content;
 
     return (
         <div className="flex flex-col bg-zinc-100">
+            <About content={ data } />
             <Experience content={ data } />
             <Education content={ data } />
             
             <section className="mt-auto flex items-start p-4 pl-8 border-t">
-                <input type="checkbox" name="afreement" checked readOnly />
+                <input type="checkbox" name="agreement" checked readOnly />
                 <p className="text-xs italic pl-2">{ data.recruitment_agreement }</p>
             </section>
         </div>
