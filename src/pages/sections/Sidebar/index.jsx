da93f647b43ca2db5707d0ad82efc29317fcf351
ArtemD <artemdanilow95@gmail.com>
2023-03-20 12:48:08 +0100

    Add max height to img element

--- src/pages/sections/Sidebar/index.jsx
+++ src/pages/sections/Sidebar/index.jsx
@@ -16,13 +16,13 @@ const Sidebar = () => {
 
     return (
         <aside className="bg-zinc-700 text-zinc-200 w-full h-full">
-            <div className="avatar hidden w-full h-[32.5rem] p-4 pb-0 md:block">
+            <div className="avatar hidden w-full h-auto p-4 pb-0 md:block">
                 <Image
                     url={ avatar }
                     width="427"
                     height="520"
                     alt="Artem Danilov portrait"
-                    classes="w-full h-full rounded-lg object-cover object-top"
+                    classes="w-full h-full max-h-[32rem] rounded-lg object-cover object-top"
                 />
             </div>
 
