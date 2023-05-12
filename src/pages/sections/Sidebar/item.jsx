e17e1d79446b4e78251b06ddd1432ce288cd5007
ArtemD <artemdanilow95@gmail.com>
2023-05-12 13:39:21 +0200

    Add Portfolio section with description and remove previos

--- src/pages/sections/Sidebar/item.jsx
+++ src/pages/sections/Sidebar/item.jsx
@@ -1,25 +1,11 @@
-import React from 'react';
-
-import { ReactComponent as Link } from '../../../assets/svg/link.svg';
+import React from "react";
 
 const Item = (props) => {
-    return (
-        <li className="text-sm font-mono lg:text-base">
-            { props.link ? (
-                <a
-                    href={ props.link }
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="link py-0.5"
-                >
-                    <Link className="inline-block pr-1 w-8 h-8 text-current"/>
-                    { props.name }
-                </a>
-            ) : (
-                <p>{ props.name }</p>
-            )}
-        </li> 
-    )
-}
+  return (
+    <li className="text-sm font-mono lg:text-base">
+      <p>{props.name}</p>
+    </li>
+  );
+};
 
 export default Item;
