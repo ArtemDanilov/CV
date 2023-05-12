c346ab1c6c1581a3550695380babd26b96b98ade
ArtemD <artemdanilow95@gmail.com>
2023-05-12 13:34:53 +0200

    Change columns width

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 import Sidebar from "./sections/Sidebar/index";
 import Main from "./sections/Main/index";
 
 const Home = () => {
-    return (
-        <main className="max-w-7xl mx-auto md:grid md:grid-cols-[35%,65%] xl:my-8 xl:shadow-lg">
-            <Sidebar />
-            <Main />
-        </main>
-    )
-}
+  return (
+    <main className="max-w-7xl mx-auto md:grid md:grid-cols-[30%,70%] xl:my-8 xl:shadow-lg">
+      <Sidebar />
+      <Main />
+    </main>
+  );
+};
 
 export default Home;
