4b40fe92b89570a30709aa676bcd8b46079d0a03
ArtemD <artemdanilow95@gmail.com>
2023-05-12 13:49:28 +0200

    Return link option to sidebar lists

--- src/pages/sections/Sidebar/item.jsx
+++ src/pages/sections/Sidebar/item.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Item = (props) => {
   return (
     <li className="text-sm font-mono lg:text-base">
-      <p>{props.name}</p>
+      {props.link ? (
+        <a
+          href={props.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link py-0.5"
+        >
+          {props.name}
+        </a>
+      ) : (
+        <p>{props.name}</p>
+      )}
     </li>
   );
 };
